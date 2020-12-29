365e43b0bcec5893376ad8f8a65b0f135d9038e5
toltarisa <isatoltar@gmail.com>
2020-12-29 11:45:39 +0300

    code wars camel case added

---
+++ src/Codewars/CamelCase.java
@@ -0,0 +1,22 @@
+package Codewars;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CamelCase {
+
+    public static String camelCase(String str) {
+
+       return (str == null || str.isEmpty())?"": Arrays.stream(str.trim().split("\\s+"))
+                .map(s -> s.substring(0,1).toUpperCase()+s.substring(1,s.length()))
+                .collect(Collectors.joining());
+       
+    }
+
+    public static void main(String[] args) {
+        String test = "ab  c";
+        String result = camelCase(test);
+
+        System.out.println(result);
+    }
+}
